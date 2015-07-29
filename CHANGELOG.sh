219c3ee0c222c44f1b613a2954c7833011170fe9
RolanDroid <rolandosimoncelli8@gmail.com>
2015-07-29 12:52:45 +0200

    Simple Changelog.sh script
    Signed-off-by: RolanDroid <rolandosimoncelli8@gmail.com>

---
+++ CHANGELOG.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Writen by RolanDroid 
+
+# how to use?
+# ./CHANGELOG.sh 
+
+echo -e '\E[37;44m'"\033[1mthis is an open source script, feel free to use and share it\033[0m"
+
+
+echo -e '\E[37;44m'"\033[1mCHANGELOG OF MonsterKernel...\033[0m"
+
+git log --oneline    

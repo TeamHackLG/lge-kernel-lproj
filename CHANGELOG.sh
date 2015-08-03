1b2ed82171176c432d348b48e7f2daef54a46213
RolanDroid <rolandosimoncelli8@gmail.com>
2015-08-03 21:57:16 +0200

    Changelog.sh updated

--- CHANGELOG.sh
+++ CHANGELOG.sh
@@ -8,6 +8,6 @@
 echo -e '\E[37;44m'"\033[1mthis is an open source script, feel free to use and share it\033[0m"
 
 
-echo -e '\E[37;44m'"\033[1mCHANGELOG OF MonsterKernel...\033[0m"
+echo -e '\E[37;44m'"\033[1mCHANGELOG OF TeamHackLG Kernel...\033[0m"
 
 git log --oneline    
